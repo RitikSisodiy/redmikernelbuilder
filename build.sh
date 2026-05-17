e698b45d16838580aa33f8513e7a8357fbd72d4f
ritik <ritik.nimoy@gemseducation.com>
2026-05-18 00:13:59 +0530

    fix

--- build.sh
+++ build.sh
@@ -98,19 +98,13 @@ apply_configs() {
     done
 }
 patch_cgroup() {
-    FILE="common/kernel/cgroup/cgroup.c"
+    FILE="kernel/cgroup/cgroup.c"
 
-    echo "Patching cgroup_setup_root safely..."
+    # Add definition at the top (after includes)
+    sed -i '1i#ifndef MY_CGROUP_SUBSYS_MASK\n#define MY_CGROUP_SUBSYS_MASK ((1 << CGROUP_SUBSYS_COUNT) - 1)\n#endif' "$FILE"
 
-    awk '
-    /cgroup_setup_root\(&cgrp_dfl_root, 0, 0\)/ {
-        print "BUG_ON(cgroup_setup_root(&cgrp_dfl_root, CGROUP_SUBSYS_MASK, 0));"
-        next
-    }
-    { print }
-    ' "$FILE" > tmp && mv tmp "$FILE"
-
-    grep -n "cgroup_setup_root" "$FILE"
+    # Replace the cgroup_setup_root call safely
+    sed -i 's/BUG_ON(cgroup_setup_root(&cgrp_dfl_root, 0, 0))/BUG_ON(cgroup_setup_root(&cgrp_dfl_root, MY_CGROUP_SUBSYS_MASK, 0))/' "$FILE"
 }
 
 getsource () {
